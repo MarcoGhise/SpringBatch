8b7a4644f4ce4ded969a8a66597b8574922d3c71
MarcoGhise <MarcoGhise@users.noreply.github.com>
2016-09-05 15:44:41 +0200

    Create Company.java

---
+++ src/main/java/it/blog/springbatch/flightradar/bean/Company.java
@@ -0,0 +1,23 @@
+package it.blog.springbatch.flightradar.bean;
+
+public class Company {
+
+	private String name;
+	private int counter;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getCounter() {
+		return counter;
+	}
+
+	public void setCounter(int counter) {
+		this.counter = counter;
+	}
+}
